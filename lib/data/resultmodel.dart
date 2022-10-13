547c919f0b11054779fef1dbf83a4f3ba291c436
outterstellar <appdevelopmentlover@gmail.com>
2022-10-13 21:01:45 +0300

    First Commit

--- lib/data/resultmodel.dart
+++
@@ -1,7 +0,0 @@
-class ResultModel{
-  String name;
-  String explanation;
-  List symptoms;
-  double percent;
-  ResultModel({required this.name,required this.explanation,required this.symptoms,required this.percent});
-}
